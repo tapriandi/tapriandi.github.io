cf1cb336bc0247288adc04dbfa14894678876d18
tapriandi <tapriandi@gmail.com>
2019-05-26 07:20:13 +0700

    update java script

--- cats gallery (tugasDOM)/script.js
+++ cats gallery (tugasDOM)/script.js
@@ -1,3 +1,5 @@
+//pop up
+
 var images = document.querySelectorAll('.gallery img');
 var modal = document.getElementById("myModal");
 
@@ -12,4 +14,55 @@ modal.addEventListener('click', function () {
   this.style.display = 'none';
 });
 
-var active = document.getElementsByClassName('active');
+//selector
+
+filterSelector("all")
+function filterSelector(select) {
+  var x, i;
+  x = document.getElementsByClassName("col");
+  if (select == "all") select = "";
+
+  for (i = 0; i < x.length; i++) {  
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(select) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+var elements = document.getElementsByClassName('col')
+// Show filtered elements
+function AddClass(elements, name) {
+  var i, arr1, arr2;
+  arr1 = elements.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      elements.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(elements, name) {
+  var i, arr1, arr2;
+  arr1 = elements.className.split(" ");
+  arr2 = name.split(" ");
+  console.log(arr1)
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+    }
+  }
+  elements.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnNav = document.getElementById("nav");
+var btn = btnNav.getElementsByClassName("btn");
+
+for (var i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
